c7e83630ab116e551d899e352bdb72525ff55644
Madias <89139100+Rtyrrx@users.noreply.github.com>
2025-10-03 05:14:41 +0500

    Delete ChatAdapter.java

--- ChatAdapter.java
+++
@@ -1,8 +0,0 @@
-package net.madias.mcds;
-
-import java.util.function.Consumer;
-
-public interface ChatAdapter {
-    void sendMessage(String message);
-    void onMessageReceived(Consumer<String> handler);
-}

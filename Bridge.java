68a3033d5bda6d2ac861d7a072b1c862ff53c6a2
Madias <89139100+Rtyrrx@users.noreply.github.com>
2025-10-03 05:14:33 +0500

    Delete Bridge.java

--- Bridge.java
+++
@@ -1,8 +0,0 @@
-package net.madias.mcds;
-
-public class Bridge {
-    public Bridge(ChatAdapter a, ChatAdapter b) {
-        a.onMessageReceived(b::sendMessage);
-        b.onMessageReceived(a::sendMessage);
-    }
-}
